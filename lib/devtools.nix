133df21caecab2a837d9d6b676c68378698b9c2c
Gustavo Stingelin <gustavo.stingelin@outlook.com>
2025-08-24 21:45:15 -0300

    add nvidia support

--- lib/devtools.nix
+++ lib/devtools.nix
@@ -1,5 +1,16 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
+  # NVIDIA drivers
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  
   #virtual box settings
    virtualisation.virtualbox.host.enable = true;
    users.extraGroups.docker.members = [ "head" ];
