91d7c9d279112528d9f04076a509db234f6d75d0
Gustavo Stingelin <gustavo.stingelin@outlook.com>
2026-07-03 22:30:20 -0300

    add zellij sessionizer

--- home-manager/zellij.nix
+++ home-manager/zellij.nix
@@ -1,7 +1,33 @@
-{ ... }:
-{
-  programs.zellij = {
-    enable = true;
+{ config, pkgs, ... }:
+let
+  home = config.home.homeDirectory;
+  sessionizer = pkgs.fetchurl {
+    url = "https://github.com/laperlej/zellij-sessionizer/releases/download/v0.5.0/zellij-sessionizer.wasm";
+    hash = "sha256-xBhBwCPnToH5mg/Y2V4FBO0gLfLNuSYE31HJ5OoLoFs=";
   };
-}
+in
+{
+  programs.zellij.enable = true;
 
+  xdg.configFile."zellij/config.kdl".text = ''
+    plugins {
+      zellij-sessionizer location="file:${sessionizer}" {
+        cwd "/"
+        root_dirs "${home}/code"
+        session_layout ":default"
+      }
+    }
+
+    keybinds {
+      tmux {
+        bind "g" {
+          LaunchOrFocusPlugin "zellij-sessionizer" {
+            floating true
+            move_to_focused_tab true
+          }
+          SwitchToMode "Locked"
+        }
+      }
+    }
+  '';
+}
