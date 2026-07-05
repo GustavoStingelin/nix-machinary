4090a8ca9e3a024e643399469de12dfdb04feee0
Gustavo Stingelin <gustavo.stingelin@outlook.com>
2026-07-04 23:05:57 -0300

    configure zjstatus layout

--- home-manager/zellij.nix
+++ home-manager/zellij.nix
@@ -1,10 +1,74 @@
 { config, pkgs, ... }:
 let
   home = config.home.homeDirectory;
+  zjstatus = pkgs.fetchurl {
+    url = "https://github.com/dj95/zjstatus/releases/download/v0.23.0/zjstatus.wasm";
+    hash = "sha256-4AaQEiNSQjnbYYAh5MxdF/gtxL+uVDKJW6QfA/E4Yf8=";
+  };
   sessionizer = pkgs.fetchurl {
     url = "https://github.com/laperlej/zellij-sessionizer/releases/download/v0.5.0/zellij-sessionizer.wasm";
     hash = "sha256-xBhBwCPnToH5mg/Y2V4FBO0gLfLNuSYE31HJ5OoLoFs=";
   };
+  layoutText = ''
+    layout {
+      tab {
+        pane
+      }
+
+      default_tab_template {
+        pane size=1 borderless=true {
+          plugin location="file:${zjstatus}" {
+            format_left "{mode}#[fg=#1E1E2E,bg=#CBA6F7,bold] {session} #[fg=#CBA6F7,bg=#1E1E2E]{tabs}"
+            format_center ""
+            format_right "{command_pwd}{command_battery}#[fg=#1E1E2E,bg=#F9E2AF]{datetime} "
+            format_space "#[bg=#1E1E2E]"
+            hide_frame_for_single_pane "false"
+
+            mode_normal "#[fg=#1E1E2E,bg=#89B4FA,bold] {name} #[fg=#89B4FA,bg=#1E1E2E]"
+            mode_tmux "#[fg=#1E1E2E,bg=#A6E3A1,bold] {name} #[fg=#A6E3A1,bg=#1E1E2E]"
+            mode_locked "#[fg=#1E1E2E,bg=#F9E2AF,bold] {name} #[fg=#F9E2AF,bg=#1E1E2E]"
+
+            tab_normal "#[fg=#1E1E2E,bg=#313244] #[fg=#CDD6F4,bg=#313244] {index} {name} #[fg=#313244,bg=#1E1E2E]"
+            tab_normal_fullscreen "#[fg=#1E1E2E,bg=#45475A] #[fg=#CDD6F4,bg=#45475A] {index} {name} #[fg=#45475A,bg=#1E1E2E]"
+            tab_normal_sync "#[fg=#1E1E2E,bg=#6C7086] #[fg=#CDD6F4,bg=#6C7086] {index} {name} #[fg=#6C7086,bg=#1E1E2E]"
+            tab_active "#[fg=#1E1E2E,bg=#89B4FA,bold,italic]{index} {name} #[fg=#89B4FA,bg=#1E1E2E]"
+            tab_active_fullscreen "#[fg=#1E1E2E,bg=#F9E2AF,bold,italic]{index} {name} #[fg=#F9E2AF,bg=#1E1E2E]"
+            tab_active_sync "#[fg=#1E1E2E,bg=#A6E3A1,bold,italic]{index} {name} #[fg=#A6E3A1,bg=#1E1E2E]"
+
+            command_pwd_command "pwd"
+            command_pwd_format "#[fg=#89B4FA] {stdout} "
+            command_pwd_interval "10"
+
+            command_battery_command "sh -c 'if command -v pmset >/dev/null 2>&1; then pmset -g batt | awk \"/%/ {sub(/^.*\\t/, \\\"\\\"); sub(/;.*$/, \\\"\\\"); print}\"; fi'"
+            command_battery_format "#[fg=#FAB387] {stdout} "
+            command_battery_interval "300"
+            command_battery_rendermode "static"
+            command_battery_hideonemptystdout "true"
+
+            datetime_timezone "America/Sao_Paulo"
+            datetime "#[fg=#F9E2AF] {format} "
+            datetime_format "%a %d %b %H:%M"
+          }
+        }
+
+        children
+      }
+
+      swap_tiled_layout name="vertical" {
+        tab split_direction="vertical" {
+          pane
+          pane
+        }
+      }
+
+      swap_tiled_layout name="horizontal" {
+        tab split_direction="horizontal" {
+          pane
+          pane
+        }
+      }
+    }
+  '';
 in
 {
   programs.zellij.enable = true;
@@ -14,7 +78,7 @@ in
       zellij-sessionizer location="file:${sessionizer}" {
         cwd "/"
         root_dirs "${home}/code"
-        session_layout ":default"
+        session_layout "${home}/.config/zellij/layouts/sessionizer.kdl"
       }
     }
 
@@ -29,5 +93,13 @@ in
         }
       }
     }
+  '';
+
+  xdg.configFile."zellij/layouts/default.kdl".text = ''
+${layoutText}
+  '';
+
+  xdg.configFile."zellij/layouts/sessionizer.kdl".text = ''
+${layoutText}
   '';
 }
